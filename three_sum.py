a2fe78600dc0e844748cfc0ad54050a06cd5c4e1
geetesh-daipuria <geeteshdaipuria@gmail.com>
2025-07-08 18:07:29 +0530

    Updated the code for three_sum problem

--- three_sum.py
+++ three_sum.py
@@ -1,15 +1,38 @@
-def triplet_sum(nums):
-    nums.sort()
-    result = []
+class Solution(object):
+    def threeSum(self,nums):
+        nums.sort()
+        result = []
+        for i in range(len(nums) - 2):
+        # Skip duplicate values for the first number
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            # Two-pointer approach
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
 
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            for k in range(j+1, len(nums)):
-                if i != j and j != k and i != k:
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        triplet = sorted([nums[i], nums[j], nums[k]])
-                        if triplet not in result:
-                            result.append(triplet)
-    return result
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    # Skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    # Move inward
+                    left += 1
+                    right -= 1
 
-print(triplet_sum([-1, 0, 1, 2, -1, -4]))
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
+
+
+s = Solution()
+
+#Test Cases
+print(s.threeSum([-1, 0, 1, 2, -1, -4]))
+print(s.threeSum([0,1,1]))
+print(s.threeSum([0,0,0]))
